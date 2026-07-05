50b73281d5e35722f7a9657343ff5b19bd75af20
ShaharAttias <attiasshahar11@gmail.com>
2026-07-05 16:39:05 +0300

    Update portal api mapping

--- portal/src/services/api.js
+++ portal/src/services/api.js
@@ -28,3 +28,23 @@ export async function registerApp(appName, appId) {
 
   return data.app;
 }
+
+export const createAppId = (name) =>
+  name
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-|-$/g, "");
+
+export const createEmptyPortalApp = (app) => ({
+  name: app.appName || app.name,
+  appId: app.appId,
+  apiKey: app.apiKey,
+
+  users: app.totalUsers || 0,
+  backups: app.totalBackups || 0,
+  devices: 0,
+  restores: 0,
+
+  usersData: [],
+});
